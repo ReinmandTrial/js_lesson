5960cfeae665c157440fdbbe2779ac70ac977062
ReinamndTrial <you@example.com>
2020-10-08 21:09:50 +0300

    trai_js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
+
+// ! Используем строгий режим 
 "use strict"
 
+// ! Выводим первую фразу в консоль,: Я учу JS 
 console.log('Я учу JS');
 
+// ! Делаем первую переменную через ключевое слово let 
+// ! и выводим её в консоль
 let denis = 'Фрилансер по жизни';
-console.log(denis + ', даёт уроки')
+console.log(denis + ', даёт уроки');
+
+// ! Делаем много переменных через запятую удобно для написания
+// ! И выводим их в консоль
+let myname = 'Maksym',
+    coffiname = 'nescafe',
+    pettyname = 'petty';
+console.log(myname, coffiname, pettyname);
+
+// ! Заменяем переменную debiAge на terryAge 
+// ! И выводим её в консоль
+let debbiAge = '21';
+let terryAge = '35';
+debbiAge = terryAge;
+console.log(debbiAge)
+
+// ! В строке 31 задана переменная с значением 21, в строке 32 задан
+// ! блок внутри которого задана эта же переменная с значением 18
+// ! выводя их в консоль мы получаем вне блока и внутри блока разные
+// ! значения 
+let myAge = '21'
+function testBlock() {
+    let myAge = 18;
+    console.log(myAge);
+}
+testBlock();
+console.log(myAge);
